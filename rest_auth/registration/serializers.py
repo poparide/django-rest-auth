76d740a93b50bf722bdc736d4852c7161bf8ba0e
Botond Béres <botondus@pm.me>
2025-06-11 15:21:01 +0300

    Updated the method to get get_app for newer django-allauth.

--- rest_auth/registration/serializers.py
+++ rest_auth/registration/serializers.py
@@ -1,13 +1,14 @@
 from django.http import HttpRequest
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import get_user_model
+from django.contrib.sites.models import Site
 
 from allauth.account import app_settings as allauth_settings
 from allauth.utils import get_username_max_length
 from allauth.account.adapter import get_adapter
 from allauth.account.utils import setup_user_email
 from allauth.socialaccount.helpers import complete_social_login
-from allauth.socialaccount.models import SocialAccount
+from allauth.socialaccount.models import SocialAccount, SocialApp
 from allauth.socialaccount.providers.base import AuthProcess
 from allauth.socialaccount.providers.oauth2.client import OAuth2Error
 from allauth.account.models import EmailAddress
@@ -70,7 +71,15 @@ class SocialLoginSerializer(serializers.Serializer):
             raise serializers.ValidationError(_("Define adapter_class in view"))
 
         adapter = adapter_class(request)
-        app = adapter.get_provider().get_app(request)
+        # The get_app method is not available anymore on social adapters
+        provider = adapter.get_provider()
+        site = Site.objects.get_current(request)
+        try:
+            app = SocialApp.objects.get(provider=provider.id, sites=site)
+        except SocialApp.DoesNotExist:
+            raise serializers.ValidationError(
+                _(f"No SocialApp configured for provider '{provider.id}' on this site.")
+            )
 
         # More info on code vs access_token
         # http://stackoverflow.com/questions/8666316/facebook-oauth-2-0-code-and-token
